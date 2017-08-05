50e2ab7b15085ecb6e12b77b8f3b2059bd3beb61
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-08-05 14:05:48 +0900

    add main

---
+++ CapsLock/src/capslock/CapsLock.java
@@ -0,0 +1,15 @@
+package capslock;
+
+/**
+ *
+ * @author RISCassembler
+ */
+public class CapsLock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
