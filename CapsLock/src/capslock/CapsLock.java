fa8a1d9ac27f9eff168bbb976b24cc4c6d7af040
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-08-16 09:28:03 +0900

    added GameCertificationClass

--- CapsLock/src/capslock/CapsLock.java
+++ CapsLock/src/capslock/CapsLock.java
@@ -1,5 +1,14 @@
 package capslock;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
  * @author RISCassembler
@@ -10,6 +19,36 @@ public class CapsLock {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        BufferedReader reader;
+        
+        try {
+            reader = new BufferedReader(new FileReader("GamesInfo.json"));
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+            return;
+        }
+        
+        String jsonString;
+        
+        try {
+            jsonString = reader.readLine();
+        } catch (IOException ex) {
+            System.out.println(ex);
+            return;
+        }
+        
+        System.out.println(jsonString);
+         
+        if(jsonString == null)return; 
+        
+        List<GameCertification> testlist = new ArrayList();
+        
+        try{
+            new JSONArray(jsonString).forEach(record -> testlist.add(new GameCertification((JSONObject) record)));
+        }catch(Exception e){
+            return;
+        }
+        testlist.forEach(ele -> ((GameCertification)ele).dump());
     }
     
 }
---
+++ CapsLock/src/capslock/GameCertification.java
@@ -0,0 +1,49 @@
+package capslock;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author RISCassembler
+ */
+
+public final class GameCertification {
+    private final UUID uuid;
+    private final String name;
+    private final Path ExecutablePath;
+    private final String version;
+    private final List<Path> ImagesPathList;
+    private final List<Path> MoviesPathList;
+    
+    public GameCertification(JSONObject record){
+        uuid = UUID.fromString(record.getString("UUID"));
+        name = record.getString("name");
+        ExecutablePath = new File(record.getString("executable")).toPath();
+        version = record.getString("version");
+        ImagesPathList = BuildImmutableArray(record.getJSONArray("image"));
+        MoviesPathList = BuildImmutableArray(record.getJSONArray("movie"));
+    }
+    
+    public void dump(){
+        System.out.println(uuid.toString());
+        System.out.println(name);
+        System.out.println(ExecutablePath.toString());
+        System.out.println(version);
+        System.out.println(ImagesPathList);
+        System.out.println(MoviesPathList);
+    }
+    
+    private List<Path> BuildImmutableArray(JSONArray DataArray){
+        ArrayList<Path> Builder = new ArrayList();
+        DataArray.forEach(file -> Builder.add(new File(file.toString()).toPath()));
+        return Collections.unmodifiableList(Builder);
+    }
+}
