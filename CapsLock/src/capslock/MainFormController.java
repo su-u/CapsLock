7becd52e24cb00edf1ca9bab399926b94bca0dbf
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-09-18 06:41:59 +0900

    If panel image isn't specified, generate that from initial of game's name.

--- CapsLock/src/capslock/MainFormController.java
+++ CapsLock/src/capslock/MainFormController.java
@@ -5,15 +5,27 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.WindowEvent;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -64,29 +76,50 @@ public class MainFormController implements Initializable {
     }
     
     public void onLoad(WindowEvent event){
-        if(!IsGameMapped){
-            final double PanelImageSideLength;
-            {
-                final double width = BaseTilePane.getWidth() / TileSizeScale;
-                final double height = BaseTilePane.getHeight() / TileSizeScale;
-                PanelImageSideLength = Double.min(width, height);
+        if(IsGameMapped)return;
+        
+        final double PanelImageSideLength;
+        {
+            final double width = BaseTilePane.getWidth() / TileSizeScale;
+            final double height = BaseTilePane.getHeight() / TileSizeScale;
+            PanelImageSideLength = Double.min(width, height);
+        }
+            
+        for(GameCertification game : GameList){
+            final Image PanelImage;
+            
+            if(Files.isRegularFile(game.getPanelPath())){
+                PanelImage = new Image(game.getPanelPath().toUri().toString());
+            }else{
+                PanelImage = GenerateCharPanel(game.getName().charAt(0));
+                LogHandler.instance.warning("game's UUID : " + game.getUUID().toString() + " doesn't have panel image.");
             }
             
-            try{
-                for(GameCertification game : GameList){
-                    ImageView view = new ImageView(new Image(game.getPanelPath().toUri().toString()));
-                    view.fitHeightProperty().setValue(PanelImageSideLength);
-                    view.fitWidthProperty().setValue(PanelImageSideLength);
-                    view.setOnMouseEntered((eve) -> {
-                        final ImageView TriggerView = (ImageView)eve.getSource();
-                        GameDisplayController.onImageFocused(TriggerView);
-                    });
-                    view.setUserData(game);
-                    BaseTilePane.getChildren().add(view);
-                }
-            }catch(Exception e){
-                System.err.println(e);
-            }
+            final ImageView view = new ImageView(PanelImage);
+            view.fitHeightProperty().setValue(PanelImageSideLength);
+            view.fitWidthProperty().setValue(PanelImageSideLength);
+            view.setOnMouseEntered((eve) -> {
+                final ImageView TriggerView = (ImageView)eve.getSource();
+                GameDisplayController.onImageFocused(TriggerView);
+            });
+            view.setUserData(game);
+            BaseTilePane.getChildren().add(view);
         }
+    }
+    
+    static private Image GenerateCharPanel(final char ch){
+        
+        final Label label = new Label(Character.toString(Character.toUpperCase(ch)));
+        label.setMinSize(125, 125);
+        label.setMaxSize(125, 125);
+        label.setPrefSize(125, 125);
+        label.setFont(Font.font(80));
+        label.setAlignment(Pos.CENTER);
+        label.setTextFill(Color.WHITE);
+        label.setBackground(new Background(new BackgroundFill(ColorSequencer.get(), CornerRadii.EMPTY, Insets.EMPTY)));
+        final Scene scene = new Scene(new Group(label));
+        final WritableImage img = new WritableImage(125, 125);
+        scene.snapshot(img);
+        return img ;
     }
 }
