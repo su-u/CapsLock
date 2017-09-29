e6fc97613c6a2dd6fce714c620be5aa0303828a7
domasyake <lehmboden.doma@gmail.com>
2017-09-29 14:24:42 +0900

    little update

--- CapsLock/src/capslock/WarningTimer.java
+++ CapsLock/src/capslock/WarningTimer.java
@@ -9,18 +9,22 @@ import javafx.util.Duration;
 public class WarningTimer {
 	private int counter=0;
 	private int popupsecond=10;
+	private boolean closed=false;
 	private OverLayWindow window=new OverLayWindow();
 	private Timeline timer=new Timeline();
 
 	public void Start() {
+		closed=false;
 		 timer = new Timeline(new KeyFrame(Duration.millis(popupsecond*1000), new EventHandler<ActionEvent>(){
 	            @Override
 	            public void handle(ActionEvent event) {
+	            	if(closed) {
 	                window.Exe();
 	                counter++;
 	                if(counter>=2) {
 	                	timer.stop();
 	                }
+	            	}
 	            }
 	        }));
 	        timer.setCycleCount(Timeline.INDEFINITE);
@@ -28,5 +32,6 @@ public class WarningTimer {
 	}
 	public void pre() {
 		counter=0;
+		closed=true;
 	}
 }
